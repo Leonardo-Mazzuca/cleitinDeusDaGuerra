44453fa30d4ee8a4b3121115bdbdba6af2840bd3
Leonardo de Souza Mazzuca <124372925+Leonardo-Mazzuca@users.noreply.github.com>
2023-10-13 11:34:54 -0300

    Update Robo.js

--- js/Robo.js
+++ js/Robo.js
@@ -12,7 +12,14 @@ const Robo = function(nome,cordX, cordY, img, velocidade, ctx, arena){
     this.moveRight = false;
     this.moveDown = false;
 
+    let partidaIniciada = false;
+    let inicioPartida;
+
     this.movimentarRoboDaDireita = function() {
+        if (!partidaIniciada) {
+            partidaIniciada = true;
+            inicioPartida = new Date().getTime();
+        }
         if (this.moveLeft && this.cordX > 0) {
             this.cordX -= this.velocidade;
         }
@@ -31,6 +38,10 @@ const Robo = function(nome,cordX, cordY, img, velocidade, ctx, arena){
     }
 
     this.movimentarRoboDaEsquerda = function() {
+        if (!partidaIniciada) {
+            partidaIniciada = true;
+            inicioPartida = new Date().getTime();
+        }
         if (this.moveLeft && this.cordX > 0) {
             this.cordX -= this.velocidade;
         }
@@ -79,54 +90,135 @@ const Robo = function(nome,cordX, cordY, img, velocidade, ctx, arena){
             collisionCooldownFrames--;
             return;
         }
-
+    
         const xColisao = Math.max(0, Math.min(this.cordX + this.img.width, outroRobo.cordX + outroRobo.img.width) - Math.max(this.cordX, outroRobo.cordX));
         const yColisao = Math.max(0, Math.min(this.cordY + this.img.height, outroRobo.cordY + outroRobo.img.height) - Math.max(this.cordY, outroRobo.cordY));
-
+    
         if (this.knockbackFrames <= 0 && xColisao > margem && yColisao > margem) {
             let xOverlap = xColisao >= yColisao ? xColisao / 2 : 0;
             let yOverlap = xColisao < yColisao ? yColisao / 2 : 0;
-
+    
             if (this.cordX + xOverlap < 0) {
                 xOverlap -= this.cordX + xOverlap;
             } else if (this.cordX + this.img.width + xOverlap > arena.width) {
                 xOverlap -= this.cordX + this.img.width + xOverlap - arena.width;
             }
-
+    
             if (this.cordY + yOverlap < 0) {
                 yOverlap -= this.cordY + yOverlap;
             } else if (this.cordY + this.img.height + yOverlap > arena.height) {
                 yOverlap -= this.cordY + yOverlap;
             }
-
+    
             if (xOverlap !== 0 || yOverlap !== 0) {
                 this.cordX -= (xOverlap * knockbackDistance / xColisao);
                 outroRobo.cordX += (xOverlap * knockbackDistance / xColisao);
                 this.cordY -= (yOverlap * knockbackDistance / yColisao);
                 outroRobo.cordY += (yOverlap * knockbackDistance / yColisao);
             }
-
+    
             this.knockbackFrames = knockbackDuration;
             outroRobo.knockbackFrames = knockbackDuration;
-
+    
             collisionCooldownFrames = collisionCooldown;
+    
             this.vida -= 1;
+            outroRobo.vida -= 1;
+    
             contadorColisoes++;
-
-            if (contadorColisoes >= maxColisoes) {
-                window.location.reload();
-                this.moveLeft = false;
-                this.moveUp = false;
-                this.moveRight = false;
-                this.moveDown = false;
-            }
+    
+            const divVidaElementoBB8 = document.querySelector('.vida-robo-BB8');
+            const divVidaElementoR2D2 = document.querySelector('.vida-robo-R2D2');
+        
+            divVidaElementoBB8.textContent = `Vida: ${this.vida}`;
+            divVidaElementoR2D2.textContent = `Vida: ${outroRobo.vida}`;
         }
-
+    
         if (this.knockbackFrames > 0) {
             this.knockbackFrames--;
-     
         }
     }
+
+    this.mostrarDadosNoFinal = function(outroRobo) {
+        const nomeRoboBB8 = document.getElementById('robo-1-nome');
+        const vidaRoboBB8 = document.getElementById('robo-1-vida');
+    
+
+        nomeRoboBB8.textContent = this.nome;
+        vidaRoboBB8.textContent = `Vida : ${Number(this.vida)}`;
+
+        const nomeRoboR2D2 = document.getElementById('robo-2-nome');
+        const vidaRoboR2D2 = document.getElementById('robo-2-vida');
+
+        nomeRoboR2D2.textContent = outroRobo.nome;
+        vidaRoboR2D2.textContent = `Vida : ${Number(outroRobo.vida)}`;
+
+        const colisoesTotais = document.getElementById('colisoes-total');
+        colisoesTotais.textContent = ` Total colisoes : ${Number(contadorColisoes)}`;
+    }
+    
+
+    this.reconheceRoboVencedor = function(outroRobo) {
+
+        const containerEstatisticas = document.querySelector('.container-estatisticas')
+        const tempoTotalPartida = document.getElementById('tempo-total');
+
+
+        const nomeVencedor = document.getElementById('vencedor-nome');
+
+
+        if (contadorColisoes >= maxColisoes) {
+            containerEstatisticas.classList.add('jogo-acabou');
+       
+
+            if (!this.vencedorDeclarado) {
+                const duracaoPartida = (new Date().getTime() - inicioPartida) / 1000; 
+                tempoTotalPartida.textContent = `Duração da partida: ${duracaoPartida.toFixed(1)} segundos`;
+
+                if (this.vida > outroRobo.vida) {  
+                    nomeVencedor.textContent = `${this.nome}`;
+
+                } else if (outroRobo.vida > this.vida) {
+                    nomeVencedor.textContent = `${outroRobo.nome}`;
+                } else {
+                    nomeVencedor.textContent = ("Empate! Ambos os robôs têm a mesma quantidade de vida.");
+                }
+                this.vencedorDeclarado = true;
+            }
+
+            this.moveLeft = false;
+            this.moveUp = false;
+            this.moveRight = false;
+            this.moveDown = false;
+            outroRobo.moveLeft = false;
+            outroRobo.moveUp = false;
+            outroRobo.moveRight = false;
+            outroRobo.moveDown = false;
+
+            setTimeout (()=> {
+
+                window.location.reload();
+
+            },3000)
+        }
+    }
+
+    this.mostrarVidaNaTela = function (outroRobo) {
+
+        const divVidaElementoBB8 = document.querySelector('.vida-robo-BB8');
+        const divVidaElementoR2D2 = document.querySelector('.vida-robo-R2D2');
+    
+        if(!this.vidaDeclarada){
+            divVidaElementoBB8.textContent = `Vida: ${this.vida}`;
+            divVidaElementoR2D2.textContent = `Vida: ${outroRobo.vida}`;
+        }
+        this.vidaDeclarada = true;
+
+    }
+
+
+    this.vencedorDeclarado = false;
+    this.vidaDeclarada = false;
 
     this.desenharRobo = function() {
         this.ctx.drawImage(this.img, this.cordX, this.cordY, this.img.width, this.img.height);
